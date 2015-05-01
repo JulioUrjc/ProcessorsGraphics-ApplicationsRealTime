7cbd074d4a42420253734576215bc6dde7d0428d
unknown <raquelpm23@gmail.com>
2015-05-01 21:58:10 +0200

    maximo y minimo y rango- 2ptosgit add .git add .

--- CUDA/ToneMapping/ToneMapping/funcHDR.cu
+++ CUDA/ToneMapping/ToneMapping/funcHDR.cu
@@ -6,7 +6,7 @@
 
 #define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
 
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 1024
 
 template<typename T>
 void check(T err, const char* const func, const char* const file, const int line) {
@@ -22,8 +22,8 @@ __global__
 void findMinMax(const float *d_in_min, const float *d_in_max, float *d_out_min, float *d_out_max)
 {
 	/// Share memory
-	__shared__ double min[BLOCK_SIZE];
-	__shared__ double max[BLOCK_SIZE];
+	__shared__ float ds_min[BLOCK_SIZE];
+	__shared__ float ds_max[BLOCK_SIZE];
 
 	/// Which thread is?
 	int t = blockIdx.x * blockDim.x + threadIdx.x;
@@ -31,8 +31,8 @@ void findMinMax(const float *d_in_min, const float *d_in_max, float *d_out_min, 
 	int t2;
 	float aux;
 
-	min[threadIdx.x] = d_in_min[t];
-	max[threadIdx.x] = d_in_max[t];
+	ds_min[threadIdx.x] = d_in_min[t];
+	ds_max[threadIdx.x] = d_in_max[t];
 
 	__syncthreads();
 
@@ -46,13 +46,13 @@ void findMinMax(const float *d_in_min, const float *d_in_max, float *d_out_min, 
 			t2 = threadIdx.x + halfPoint;
 
 			// Get the shared value stored by another thread
-			aux = min[t2];
-			if (aux < min[threadIdx.x])
-				min[threadIdx.x] = aux;
+			aux = ds_min[t2];
+			if (aux < ds_min[threadIdx.x])
+				ds_min[threadIdx.x] = aux;
 
-			aux = max[t2];
-			if (aux > max[threadIdx.x])
-				max[threadIdx.x] = aux;
+			aux = ds_max[t2];
+			if (aux > ds_max[threadIdx.x])
+				ds_max[threadIdx.x] = aux;
 		}
 		__syncthreads();
 
@@ -61,33 +61,34 @@ void findMinMax(const float *d_in_min, const float *d_in_max, float *d_out_min, 
 	}
 	//Save the min and max at the block
 	if (threadIdx.x == 0){
-		d_out_min[blockIdx.x] = min[0];
-		d_out_max[blockIdx.x] = max[0];
+		d_out_min[blockIdx.x] = ds_min[0];
+		d_out_max[blockIdx.x] = ds_max[0];
 	}
 
 }
 
-
 void calculate_cdf(const float* const d_logLuminance, unsigned int* const d_cdf, float &min_logLum, float &max_logLum, const size_t numRows, const size_t numCols, const size_t numBins)
 {
+	/// Calcule the size of grid and block
 	int blockSize = BLOCK_SIZE;
-	int gridSize = (numRows*numCols) / BLOCK_SIZE;
+	int gridSize = ceil((float)(numRows*numCols) / (float) BLOCK_SIZE); // Upper round
 
 	///	1) Encontrar el valor máximo y mínimo de luminancia en min_logLum and max_logLum a partir del canal logLuminance
 
-	/// Variables to calculate the max and min values and allocate the memory for it.
+	/// Declare and allocate variables to calculate the max and min values and allocate the memory for it.
 	float *d_min, *d_max, *d_min_aux, *d_max_aux;
-	checkCudaErrors(cudaMalloc(&d_min0, sizeof(float) * BLOCK_SIZE));
-	checkCudaErrors(cudaMalloc(&d_max0, sizeof(float) * BLOCK_SIZE));
+	checkCudaErrors(cudaMalloc(&d_min, sizeof(float) * BLOCK_SIZE));
+	checkCudaErrors(cudaMalloc(&d_max, sizeof(float) * BLOCK_SIZE));
 
 	/// Execute kernels
 	findMinMax << <gridSize, blockSize >> >(d_logLuminance, d_logLuminance, d_min, d_max);
+	printf("Raquel => min = %d  -- max = %d\n", d_min, d_max);
 
 	while (gridSize>1)
 	{
 		//Se reduce el tamaño del grid BLOCKSIZE veces
-		gridSize = gridSize / BLOCK_SIZE;
-
+		gridSize = ceil((float)gridSize / (float)BLOCK_SIZE);
+		
 		//Iteración Minimo
 		checkCudaErrors(cudaMalloc(&d_min_aux, sizeof(float)*gridSize));
 		checkCudaErrors(cudaMalloc(&d_max_aux, sizeof(float)*gridSize));
@@ -100,16 +101,30 @@ void calculate_cdf(const float* const d_logLuminance, unsigned int* const d_cdf,
 	}
 
 	/// Copy to Host the results of max and min and clean memory
-	checkCudaErrors(cudaMemcpy(&min_logLum, d_min, sizeof(float), cudaMemcpyDeviceToHost));
-	checkCudaErrors(cudaMemcpy(&max_logLum, d_max, sizeof(float), cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(&min_logLum, d_min_aux, sizeof(float), cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(&max_logLum, d_max_aux, sizeof(float), cudaMemcpyDeviceToHost));
 
+	/// Free memory
 	checkCudaErrors(cudaFree(d_min));
 	checkCudaErrors(cudaFree(d_max));
+	checkCudaErrors(cudaFree(d_min_aux));
+	checkCudaErrors(cudaFree(d_max_aux));
 
-	//2) Obtener el rango a representar
+	///	2) Obtener el rango a representar
+	float range = max_logLum - min_logLum;
 
 	//3) Generar un histograma de todos los valores del canal logLuminance usando la formula
 	//		bin = (Lum [i] - lumMin) / lumRange * numBins
+
+	/// Recalcule the size of grid and block
+	gridSize = ceil((float)(numRows*numCols) / (float)BLOCK_SIZE);
+	blockSize = BLOCK_SIZE;
+
+	/// Declare, allocate and zero histogram variable
+	int* d_histogram;
+	cudaMalloc(&d_histogram, sizeof(int)*numBins);
+	cudaMemset(d_histogram, 0, sizeof(int)*numBins);
+
 
 	//4) Realizar un exclusive scan en el histograma para obtener la distribución acumulada (cdf)
 	//	de los valores de luminancia. Se debe almacenar en el puntero c_cdf
